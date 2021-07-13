580d9d6a6e3ba86e309d6e9fae9a0dd6e07d5e68
Your Name <you@example.com>
2021-07-13 13:31:40 +0800

    java and Kotlin variable.data type

--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -2,9 +2,12 @@ package com.tom
 
 fun main() {
 //    println("Hello kotlin")
-//    Human().hello()
-    val h=Human()
-    h.hello()
+    Human().hello()
+    var age=19; //變數型態的自動推斷
+    age=20;
+    var weight=66.5f
+    var name:String
+    name="Hank"
 }
 
 class Human{
