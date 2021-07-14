51b1da6e0992c7d6ce3c9b8f8fe0e5114218de83
Your Name <you@example.com>
2021-07-14 19:47:56 +0800

    1~18章

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -2,18 +2,28 @@ package com.tom;
 
 public class Hello {
     public static void main(String[] args) {
-    //  System.out.println("Hello world");
-        Person p=new Person();
+        //  System.out.println("Hello world");
+        String s = "abcde";
+        System.out.println(s.charAt(3));
+        System.out.println(s.substring(3));
+        System.out.println(s.substring(1, 4));
+        Person p = new Person("Tom", 66.5f, 1.7f);
         p.hello();
-        int age =19;
+        p.weight = 66.5f;
+        p.height = 1.7f;
+        System.out.println(p.bmi());
+        int score = 88;
+        System.out.println(score < 80 || score > 90);
+        char c = 'A'; //A的Unicode大小為65,a則為97
+        System.out.println(c > 'a');
+ /*       int age =19;
         Integer age2=19;
-        char c='我';
         Character c2='A';
         byte b=120;
         float weight=66.5f;
         double height=1.7;
         boolean adult=true;
         boolean enroll=false;
-        String name="Tom";
+        String name="Tom";*/
     }
 }
--- src/main/java/com/tom/Person.java
+++ src/main/java/com/tom/Person.java
@@ -1,6 +1,25 @@
 package com.tom;
 
-public class Person {
+public class Person extends Object {
+    String name;
+    float weight;
+    float height;
+
+    public Person(float weight, float height) {  //方法內的區域變數
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public Person(String name, float weight, float height) {
+        this(weight, height);  //這一定要在第一行
+        this.name = name;
+    }
+
+    public float bmi() {
+        float bmi = weight / (height * height);
+        return bmi;
+    }
+
     public void hello() {
         System.out.println("Hello world");
     }
